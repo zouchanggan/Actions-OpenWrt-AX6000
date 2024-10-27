46913395c361889ac7a4c64650a00bfff1ed64dd
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-10-27 19:51:41 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
@@ -40,7 +40,7 @@ rm -rf package/OpenAppFilter
 rm -rf package/lucky
 rm -rf package/luci-app-ddns-go
 rm -rf package/mosdns
-
+ 
 #添加额外软件包
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone https://github.com/zouchanggan/mbedtls.git package/libs/mbedtls
@@ -56,40 +56,41 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # 删除软件包
 # rm -rf package/passwall-packages/shadowsocksr-libev
 rm -rf package/passwall-packages/shadowsocks-rust
-rm -rf package/helloworld/shadowsocks-rust
-rm -rf package/helloworld/v2raya
-rm -rf package/helloworld/chinadns-ng
-rm -rf package/helloworld/mosdns
-rm -rf package/helloworld/gn
+rm -rf package/passwall-packages/v2ray-geodata
+rm -rf package/passwall-packages/chinadns-ng
 rm -rf package/passwall-packages/gn
-cp -r package/small/gn feeds/packages/net
 
 # 替换软件&依赖
+# cp -r package/SSRP/update/shadowsocksr-libev package/passwall-packages
+# cp -r package/SSRP/update/shadowsocks-rust package/passwall-packages
+cp -r package/SSRP/update/v2raya package/passwall-packages
+cp -r package/SSRP/themes/luci-theme-design feeds/luci/themes
 cp -r package/small/shadowsocks-rust package/passwall-packages
-cp -r package/small/shadowsocks-rust package/helloworld
-cp -r package/SSRP/update/v2raya package/helloworld
+cp -r package/helloworld/luci-app-ssr-plus package/passwall-packages
 cp -r package/passwall/luci-app-passwall package/passwall-packages
 cp -r package/passwall2/luci-app-passwall2 package/passwall-packages
 cp -r package/small/luci-app-mosdns package/passwall-packages
 cp -r package/small/mosdns package/passwall-packages
+cp -r package/small/v2ray-geodata package/passwall-packages
+cp -r package/small/chinadns-ng package/passwall-packages
+cp -r package/small/v2dat package/passwall-packages
+cp -r package/small/lua-neturl package/passwall-packages
+cp -r package/small/redsocks2 package/passwall-packages
+cp -r package/small/shadow-tls package/passwall-packages
 cp -r package/small/luci-app-openclash package/passwall-packages
-cp -r package/small/chinadns-ng package/helloworld
-cp -r package/small/gn package/helloworld
 cp -r package/small/gn package/passwall-packages
-cp -r package/small/mosdns package/helloworld
 
 # 删除软件包
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-appfilter
-rm -rf feeds/luci/applications/luci-app-ddns-go
-rm -rf feeds/luci/applications/luci-app-lucky
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf package/SSRP
 rm -rf package/small
 rm -rf package/passwall
 rm -rf package/passwall2
+rm -rf package/helloworld
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
