ad4bd378b986e12c36840005354a40537df77acf
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-10-27 20:34:47 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
@@ -35,6 +35,7 @@ rm -rf feeds/packages/net/gn
 rm -rf feeds/packages/lang/golang
 rm -rf package/libs/mbedtls
 rm -rf feeds/packages/net/ddns-go
+rm -rf feeds/luci/themes
 
 #添加额外软件包
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
@@ -56,7 +57,7 @@ rm -rf package/passwall-packages/gn
 
 # 替换软件&依赖
 cp -r package/SSRP/update/v2raya package/passwall-packages
-cp -r package/SSRP/themes/luci-theme-design feeds/luci/themes
+cp -r package/SSRP/themes feeds/luci
 cp -r package/small/shadowsocks-rust package/passwall-packages
 cp -r package/helloworld/luci-app-ssr-plus package/passwall-packages
 cp -r package/passwall/luci-app-passwall package/passwall-packages
@@ -71,7 +72,6 @@ cp -r package/small/redsocks2 package/passwall-packages
 cp -r package/small/shadow-tls package/passwall-packages
 cp -r package/small/luci-app-openclash package/passwall-packages
 cp -r package/small/gn package/passwall-packages
-cp -r package/small/gn feeds/packages/net
 
 # 删除软件包
 rm -rf feeds/luci/applications/luci-app-passwall
