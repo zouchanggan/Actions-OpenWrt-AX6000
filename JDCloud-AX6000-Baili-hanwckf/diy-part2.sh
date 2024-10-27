30e2839ce06f4ee66dd750ff7330d089a4201d05
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-10-27 18:04:03 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
@@ -59,7 +59,9 @@ rm -rf package/passwall-packages/shadowsocks-rust
 rm -rf package/helloworld/shadowsocks-rust
 rm -rf package/helloworld/v2raya
 rm -rf package/helloworld/chinadns-ng
+rm -rf package/helloworld/mosdns
 rm -rf package/helloworld/gn
+rm -rf package/passwall-packages/gn
 
 # 替换软件&依赖
 cp -r package/small/shadowsocks-rust package/passwall-packages
@@ -72,6 +74,8 @@ cp -r package/small/mosdns package/passwall-packages
 cp -r package/small/luci-app-openclash package/passwall-packages
 cp -r package/small/chinadns-ng package/helloworld
 cp -r package/small/gn package/helloworld
+cp -r package/small/gn package/passwall-packages
+cp -r package/small/mosdns package/helloworld
 
 # 删除软件包
 rm -rf feeds/luci/applications/luci-app-passwall
