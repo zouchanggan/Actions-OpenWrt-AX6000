2ba26a8624c2e82891c2f677284f3e78551fbc80
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-02-23 16:07:13 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
